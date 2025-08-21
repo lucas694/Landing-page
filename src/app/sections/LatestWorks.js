3b138722e96c186e471425da2423874d79b68b4d
lucas694 <lucasassuncao694@gmail.com>
2025-08-21 11:06:46 -0300

    d

--- src/app/sections/LatestWorks.js
+++ src/app/sections/LatestWorks.js
@@ -7,7 +7,7 @@ const LatestWorks = () => {
         <div className='w-full flex-col items-center px-6 py-10 lg:py-20 '>
             <div className="w-full flex flex-col text-center">
                 <h1 className='text-[#2D2D2D] text-4xl font-bold'>Discover our latest work</h1>
-                <p className='text-[#2D2D2D] text-base  font-normal mt-4 md:text-lg'>Explore our portfolio and see the latest and greatest projects that we've brought to life.</p>
+                <p className='text-[#2D2D2D] text-base  font-normal mt-4 md:text-lg'>Explore our portfolio and see the latest and greatest projects that we&apos;ve brought to life.</p>
             </div>
             <div className='w-full flex flex-col items-center mt-10 space-y-6 lg:space-y-0 lg:flex-row lg:justify-around lg:space-x-6'>
                 {LatestWorkdsData.slice(0, 3).map((work) => (
